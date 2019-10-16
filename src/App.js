5baeacc3cff6c91fc3653987492238344aedd289
Laura Shamus <lkshamus@gmail.com>
2019-10-16 13:32:44 -0600

    Add GET request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { key } from "./apiKey"
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     this.state = {
       origin: "",
       destination: "",
-      exchangeRate: ""
+      exchangeRate: {}
     };
   }
 
@@ -16,23 +17,34 @@ class App extends Component {
     this.setState({[name]: value })
   }
 
+  submitForm = async (event, apiKey) => {
+    event.preventDefault()
+    const data = await fetch(`https://free.currconv.com/api/v7/convert?q=${this.state.origin}_${this.state.destination}&compact=ultra&apiKey=${key}`);
+    const response = await data.json();
+    this.setState({exchangeRate: response})
+    return response;
+  }
+
   render() {
     return (
-      <form>
-        <input 
-          name="origin"
-          value={this.state.origin}
-          placeholder="current currency"
-          onChange={this.updateInput}
-          />
-        <input 
-          name="destination"
-          value={this.state.destination}
-          placeholder="convert to"
-          onChange={this.updateInput}
-          />
-        <button> submit </button>
-      </form>
+      <div>
+        <form onSubmit={this.submitForm}>
+          <input 
+            name="origin"
+            value={this.state.origin}
+            placeholder="current currency"
+            onChange={this.updateInput}
+            />
+          <input 
+            name="destination"
+            value={this.state.destination}
+            placeholder="convert to"
+            onChange={this.updateInput}
+            />
+          <button> submit </button>
+        </form>
+        <p>{Object.values(this.state.exchangeRate)}</p>
+      </div>
     )
   }
 }

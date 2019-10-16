b8a2d12717a9c7f8261a11cc8b889640bc6a2cc7
Laura Shamus <lkshamus@gmail.com>
2019-10-16 14:46:03 -0600

    Add option for user to input country name instead of currency abbreviation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { key } from "./apiKey"
+import { key, currencySymbols } from "./apiKey"
+import { countries } from "./countriesList"
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      countryList: {},
       origin: "",
       destination: "",
       exchangeRate: {}
@@ -14,15 +16,36 @@ class App extends Component {
 
   updateInput = (event) => {
     const { name, value } = event.target
+    if (value.length >= 3) {
+      this.checkInput()
+    }
     this.setState({[name]: value })
   }
 
+  checkInput = () => {
+    let country = countries.find((x) => {
+      return (this.state.origin === x.name || this.state.origin === x.abbr) || (this.state.destination === x.name || this.state.destination === x.abbr)
+    })
+    if (country) {
+      this.setState({origin: country.abbr, destination: country.abbr})
+    }
+  }
+
   submitForm = async (event, apiKey) => {
+    console.log(this.state.origin)
     event.preventDefault()
     const data = await fetch(`https://free.currconv.com/api/v7/convert?q=${this.state.origin}_${this.state.destination}&compact=ultra&apiKey=${key}`);
     const response = await data.json();
     this.setState({exchangeRate: response})
+    console.log(currencySymbols.USD)
     return response;
+  }
+
+  getCountryData = async () => {
+    const data = await fetch(`https://free.currconv.com/api/v7/countries?&apiKey=${key}`);
+    const response = await data.json();
+    this.setState({countryList: response.results})
+    return response.results
   }
 
   render() {
